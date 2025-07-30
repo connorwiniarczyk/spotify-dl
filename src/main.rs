5829d731f8e6a1160bb51b61c8f5db051699e65a
connor <connor>
2025-07-30 00:28:22 +0000

    new custom backend that doesn't require launching player in a separate process

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,18 @@ use librespot::oauth;
 use librespot::core::spotify_id::SpotifyId;
 use librespot::core::spotify_id::SpotifyItemType;
 
-use librespot::playback::audio_backend;
 use librespot::playback::config as playback_config;
 use librespot::playback::player;
+use librespot::playback::player::PlayerEvent;
 use librespot::playback::mixer;
 
-use std::time;
-
 use futures_executor::block_on;
-
-use std::process::Stdio;
-use std::process::Command;
 
 use librespot::metadata::{
     Metadata,
     Playlist,
     Track,
-    // Album,
+    Album,
     // Artist,
 };
 
@@ -34,9 +29,8 @@ use std::env;
 
 use tokio;
 
-fn get_stored_access_token() -> Option<String> {
-	return std::fs::read_to_string("access_token.txt").ok();
-}
+mod record;
+use record::RecordSink;
 
 fn parse_spotify_link(input: &str) -> Result<SpotifyId, ()> {
     lazy_static! {
@@ -89,122 +83,91 @@ fn try_manual_login(session: &Session) -> Result<(), ()> {
     Ok(())
 }
 
-fn run_track_downloader(id: SpotifyId, session: &Session) -> Result<(), ()> {
-    let track = block_on(Track::get(session, &id)).or(Err(()))?;
-
-    let mut artists = String::new();
-
-    let mut i = 0;
-    while i < track.artists.len() {
-        artists.push_str(track.artists[i].name.as_str());
-        i += 1;
-        if i < track.artists.len() {
-            artists.push_str(",");
-        }
-    }
-
-    let uri = format!("spotify:track:{}", id.to_base62().unwrap());
-    let output_path = format!("{}.ogg", track.name);
-
-    println!("downloading {} [{}]", track.name, artists);
-
-    // let mut downloader = Command::new("../target/debug/spotify-dl-downloader")
-    let mut downloader = Command::new("spotify-dl-downloader")
-        .arg(uri)
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to open process");
-
-    let downloader_out: Stdio = downloader
-        .stdout
-        .take()
-        .unwrap()
-        .try_into()
-        .unwrap();
-
-    let mut converter = Command::new("ffmpeg")
-        .arg("-f").arg("ogg")
-        .arg("-i").arg("pipe:")
-        .arg("-metadata").arg(&format!("title={}", track.name))
-        .arg("-metadata").arg(&format!("album={}", track.album.name))
-        .arg("-metadata").arg(&format!("artist={}", artists))
-        .arg(output_path)
-        .stdin(downloader_out)
-        // .stdout(Stdio::null())
-        // .stderr(Stdio::null())
-        .spawn()
-        .expect("failed to open ffmpeg");
-
-	let mut downloader_done = false;
-	let mut converter_done = false;
-
-	let time_begin = std::time::Instant::now();
-
-	while !downloader_done && !converter_done {
-    	match downloader.try_wait() {
-        	Ok(Some(status)) => {
-            	downloader_done = true;
-            	if !status.success() {
-                	converter.kill();
-            	}
-        	},
-        	Ok(None)    => { },
-        	Err(e)      => { println!("error: {:?}", e); },
-    	};
-
-    	match converter.try_wait() {
-        	Ok(Some(status)) => {
-            	converter_done = true;
-            	if !status.success() {
-                	downloader.kill();
-            	}
-        	},
-        	Ok(None)    => { },
-        	Err(e)      => { },
-    	};
-
-    	if time_begin.elapsed().as_secs() > 20 {
-        	println!("timed out, aborting");
-        	converter.kill();
-        	downloader.kill();
-        	return Err(());
-    	}
-
-    	std::thread::sleep(time::Duration::from_millis(100));
-	}
-
-	return Ok(())
-}
-
 fn get_tracks_to_download(id: SpotifyId, session: &Session) -> Vec<SpotifyId> {
     let mut output = Vec::new();
 
 	match id.item_type {
     	SpotifyItemType::Playlist => {
-
             let playlist = block_on(Playlist::get(&session, &id)).expect("failed to fetch playlist");
+            println!("{:#?}", playlist);
+
             let mut count = 0;
-
-            let _ = std::fs::create_dir(&playlist.name());
-            env::set_current_dir(env::current_dir().unwrap().join(&playlist.name()));
-
 			for track in playlist.tracks() {
     			output.push(*track);
     			count += 1;
 			}
             println!("found playlist: {} with {} songs", playlist.name(), count);
-
     	},
-    	SpotifyItemType::Album    => println!("album"),
-    	SpotifyItemType::Artist   => println!("artist"),
-    	SpotifyItemType::Track    => println!("track"),
+
+    	SpotifyItemType::Album => {
+            let album = block_on(Album::get(&session, &id)).expect("failed to fetch playlist");
+            println!("{:#?}", album);
+            let mut count = 0;
+			for track in album.tracks() {
+    			output.push(*track);
+    			count += 1;
+			}
+            println!("found album: {} with {} songs", album.name, count);
+    	},
+
+    	SpotifyItemType::Track    => {
+        	output.push(id);
+    	},
+
     	SpotifyItemType::Episode  => println!("episode"),
     	SpotifyItemType::Show     => println!("show"),
     	SpotifyItemType::Local    => println!("local"),
+    	SpotifyItemType::Artist   => println!("artist"),
     	SpotifyItemType::Unknown  => println!("unknown"),
 	}
 
 	return output;
+}
+
+async fn record_track(track: SpotifyId, session: Session) -> Result<(), ()> {
+    let mut player_config = playback_config::PlayerConfig::default();
+    player_config.passthrough = true;
+    let metadata = Track::get(&session, &track).await.unwrap();
+
+    let path = format!("spotify-dl/{}.ogg", track.to_base62().unwrap());
+
+    if std::fs::Path::exists(path) {
+        println!("skipping {}, already exists");
+    }
+
+    let player = player::Player::new(player_config, session, Box::new(mixer::NoOpVolume), move || {
+        RecordSink::create(metadata)
+    });
+
+    player.load(track, true, 0);
+
+    let mut channel = player.get_player_event_channel();
+    while let Some(event) = channel.recv().await {
+        match event {
+            PlayerEvent::Playing      {..} => {},
+            PlayerEvent::TrackChanged {..} => {},
+            PlayerEvent::Unavailable  {..} => {
+                println!("spotify:track:{} is unavailable, aborting", track.to_base62().unwrap());
+                break;
+            },
+
+            PlayerEvent::Paused {..} => {
+                println!("player paused, aborting");
+                player.stop();
+                break;
+            },
+
+            PlayerEvent::EndOfTrack {..} => {
+                player.stop();
+                break;
+            },
+
+            PlayerEvent::TimeToPreloadNextTrack {..} => (),
+            event => println!("{:?}", event),
+        }
+    }
+
+    Ok(())
 }
 
 #[tokio::main]
@@ -221,11 +184,19 @@ async fn main() {
         try_manual_login(&session).expect("failed to login");
     }
 
+    let _ = std::fs::create_dir("spotify-dl");
+
 	let id = parse_spotify_link(&arg).expect("invalid link");
 	let tracks = get_tracks_to_download(id, &session);
 
+	println!("downloading {} tracks with ids:", tracks.len());
+	for id in tracks.iter() {
+    	println!("spotify:track:{}", id.to_base62().unwrap());
+	}
+	println!();
+
 	for id in tracks {
-        if run_track_downloader(id, &session).is_err() {
+        if record_track(id, session.clone()).await.is_err() {
             println!("failed");
         }
 	}
---
+++ src/record.rs
@@ -0,0 +1,93 @@
+
+
+
+use librespot::playback::audio_backend::{ Sink, SinkError, SinkResult};
+
+
+// use librespot::playback::config::AudioFormat;
+use librespot::playback::decoder::AudioPacket;
+use librespot::playback::convert::Converter;
+
+use librespot::metadata::{
+    // Metadata,
+    // Playlist,
+    Track,
+    // Album,
+    // Artist,
+};
+
+use std::process::Stdio;
+use std::process::Command;
+use std::io::Write;
+
+pub struct RecordSink {
+    process: std::process::Child,
+    stream:  std::process::ChildStdin,
+}
+
+impl RecordSink {
+
+    fn get_artists_string(track: &Track) -> String {
+		let mut artists = String::new();
+        let mut i = 0;
+        while i < track.artists.len() {
+            artists.push_str(track.artists[i].name.as_str());
+            i += 1;
+            if i < track.artists.len() {
+                artists.push_str(", ");
+            }
+        }
+
+        return artists
+    }
+
+    pub fn create(track: Track) -> Box<dyn Sink> {
+        let path = format!("spotify-dl/{}.ogg", track.id.to_base62().unwrap());
+        let artists = Self::get_artists_string(&track);
+
+        let mut process = Command::new("ffmpeg")
+            .arg("-f").arg("ogg")
+            .arg("-i").arg("pipe:")
+            .arg("-metadata").arg(&format!("title={}", track.name))
+            .arg("-metadata").arg(&format!("album={}", track.album.name))
+            .arg("-metadata").arg(&format!("artist={}", artists))
+            .arg(path)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .spawn()
+            .expect("failed to open ffmpeg");
+
+        let stream = process.stdin.take().unwrap();
+        let output = Self { process, stream };
+        Box::new(output)
+    }
+}
+
+impl Sink for RecordSink {
+    fn start(&mut self) -> SinkResult<()> {
+        Ok(())
+    }
+
+    fn stop(&mut self) -> SinkResult<()> {
+        if let Err(_) = self.process.wait() {
+            return Err(SinkError::OnWrite("failed to await ffmpeg".to_owned()));
+        }
+
+        Ok(())
+    }
+
+    fn write(&mut self, packet: AudioPacket, _conveter: &mut Converter) -> SinkResult<()> {
+        let AudioPacket::Raw(bytes) = packet else {
+            panic!("found non-raw samples");
+        };
+
+        if self.stream.write(&bytes).is_err() {
+            return Err(SinkError::OnWrite("failed to write bytes to ffmpeg".to_owned()));
+        };
+
+        Ok(())
+    }
+}
+
+
